0a796f29659ca3b2942fd730a6aecd0906e71abf
9890ABHI <abhipawar7588@gmail.com>
2024-08-13 12:38:59 +0530

    update

---
+++ ansi/runner.sh
@@ -0,0 +1,25 @@
+
+
+
+# hadoop_version: "3.3.5"
+
+# hadoop_user: hadoop
+
+# hadoop_group: hadoop
+
+# hadoop_dir: "/opt/hadoop"
+
+
+
+# ansible-playbook provisioning.yml
+
+
+
+
+
+# ansible-playbook -i inventory hadoop-install.yml
+
+
+#  /opt/hadoop/hadoop-3.3.5/bin/hdfs namenode -format
+
+
